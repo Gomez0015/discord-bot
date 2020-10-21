509c00b849afd53dfa68c8187c5614c7597dfffd
Gomez0015 <oscar.gomez008@gmail.com>
2020-10-21 10:39:21 +0100

    Added simple commands and xied marys shit

---
+++ Commands.js
@@ -0,0 +1,28 @@
+const { musicLink } = require('./bot.js');
+
+exports.basicResponses =  [
+    {
+        question: "xtest",
+        answer: "u suck balls"
+    },
+    {
+        question: "xporn",
+        answer: "https://img-hw.xnxx-cdn.com/videos/thumbs169xnxxll/3d/d6/52/3dd65279490284331dde57c24115f99e/3dd65279490284331dde57c24115f99e.7.jpg"
+    },
+    {
+        question: "xtime",
+        answer: "The UTC time is: " + new Date().getUTCHours() + ":" + new Date().getUTCMinutes() + ":" + new Date().getUTCSeconds()
+    },
+    {
+        question: "xnigger",
+        answer: "no u"
+    },
+    {
+        question: "xwhy does sami exist",
+        answer: "cause he bombed pearl harbor."
+    },
+    {
+        question: "xdrawinggame",
+        answer: "https://sketchful.io/"
+    }
+];
--- bot.js
+++ bot.js
@@ -1,6 +1,12 @@
 // Require Modules
 const Discord = require("discord.js");
 const cron = require('node-cron');
+const commands = require('./Commands.js');
+
+//Exports
+let basicResponseArray = commands.basicResponses;
+
+prefix = process.env.BOT_PREFIX || "x";
 
 // Create a discord client.
 const client = new Discord.Client();
@@ -13,6 +19,7 @@ client.once('ready', () => {
     console.log("Ready!");
 });
 
+//Mary Pill Reminder Start
 client.once('ready', () => {
     cron.schedule('*/5 9 * * *', () => {
         reminderTimer();
@@ -20,14 +27,14 @@ client.once('ready', () => {
 });
 
 client.once('ready', () => {
-    cron.schedule('0 9 * * *', () => {
-        console.log("its 9 am.");
+    cron.schedule('0 7 * * *', () => {
+        console.log("its 9 am...");
         reminderTimer();
     });
 });
 
 function reminderTimer(){
-    console.log("Sending Reminders..."); 
+    console.log("Sending Reminder..."); 
     if(stopReminder == false){  
         client.users.cache.get("366327612014067722").send("Take ur pills. Its an order.");
     }        
@@ -45,11 +52,33 @@ client.on('message', msg => {
 
 cron.schedule('0 6 * * *', () => {
     stopReminder = false;
-    console.log("Stop Reminder Set to false")
-  }, {
-    scheduled: true,
-    timezone: "Africa/Casablanca"
+    console.log("Stop Reminder Set to false...")
+});
+//Mary Pill Reminder End
+
+//Welcome msg
+client.on('guildMemberAdd', member => {
+    member.guild.channels.get('758793497246957629').send("Welcum to da sever m8"); 
+});
+
+//Basic Commands
+client.on('message', msg => {
+    if (msg.author.bot) return;
+    if (msg.channel.type == "text") {
+        basicResponseArray.forEach(item => {
+            if(msg.content === item.question){
+                msg.reply(item.answer);
+            }          
+        });         
+    }
+});
+
+client.once('reconnecting', () => {
+ console.log('Reconnecting!');
+});
+client.once('disconnect', () => {
+ console.log('Disconnect!');
 });
 
 // Use token to login to the bot.
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN || "NzU4Nzg5Njc3OTgwOTc1MTQ0.X20D9A.2lTe1srYHaN7C5XOVmDgg4t0i3o");
