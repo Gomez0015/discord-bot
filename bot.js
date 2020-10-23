070f27c0526fa6d8d511412e99ab0d65bc600cd2
Gomez0015 <oscar.gomez008@gmail.com>
2020-10-23 20:58:43 +0100

    added welcome stuff

--- bot.js
+++ bot.js
@@ -59,7 +59,7 @@ cron.schedule('0 6 * * *', () => {
 
 //Welcome msg
 client.on('guildMemberAdd', member => {
-    member.guild.channels.cache.get('769280253268459520').send(member.displayName + " Welcome to the sever! Read #please-read-first to verify."); 
+    member.guild.channels.cache.get('769280253268459520').send(member.nickname + " Welcome to the sever! Read #please-read-first to verify."); 
     var role = member.guild.roles.cache.find(role => role.name === "Not Verified");
     member.roles.add(role);
 });
